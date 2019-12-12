6261f45c519806abb7a88079b2a1695b35536d1f
Fetolipe <felipe.monteiro@novaandradina.org>
2019-12-12 13:13:09 -0300

    first commit

---
+++ src/data/products.js
@@ -0,0 +1,67 @@
+const data = [
+  {
+    id: 1,
+    name: 'Vagabond VOL 1',
+    price: 40.99,
+    currency: 'BRL',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51ZIlvl3E0L._SX341_BO1,204,203,200_.jpg',
+  },
+  {
+    id: 2,
+    name: 'Vagabond VOL 2',
+    price: 30.00,
+    currency: 'BRL',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51GOdtRiziL._SX341_BO1,204,203,200_.jpg',
+  },
+  {
+    id: 4,
+    name: 'Vagabond VOL 3',
+    price: 21.99,
+    currency: 'BRL',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51nbayM-yPL._SX349_BO1,204,203,200_.jpg',
+  },
+  {
+    id: 6,
+    name: 'Vagabond VOL 4',
+    price: 15.99,
+    currency: 'BRL',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51JyN%2BR2u9L._SX339_BO1,204,203,200_.jpg',
+  },
+  {
+    id: 5,
+    name: 'Vagabond VOL 5',
+    price:13.99,
+    currency: 'BRL',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51rIHon50xL._SX342_BO1,204,203,200_.jpg',
+  },
+  {
+    id: 3,
+    name: '',
+    price: 13.99,
+    currency: 'BRL',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51dj4K51zHL._SX342_BO1,204,203,200_.jpg',
+  },
+  {
+    id: 7,
+    name: '',
+    price: 13.99,
+    currency: 'BRL',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51dj4K51zHL._SX342_BO1,204,203,200_.jpg',
+  },
+  {
+    id: 8,
+    name: '',
+    price: 13.99,
+    currency: 'BRL',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51dj4K51zHL._SX342_BO1,204,203,200_.jpg',
+  },
+  {
+    id: 9,
+    name: '',
+    price: 13.99,
+    currency: 'BRL',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51dj4K51zHL._SX342_BO1,204,203,200_.jpg',
+  }
+];
+
+export default data;
